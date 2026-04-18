3e05faafe76bdb03a28c8db94c50db5887b9f169
Nelson <nelsonjosuemf15@gmail.com>
2026-04-18 12:43:17 -0600

    refactor(database): corregir enum role y valor por defecto en users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
 
-            $table->string('role')->nullable();
+            $table->enum('role', ['admin', 'agent', 'client'])->default('client');
 
             $table->rememberToken();
             $table->timestamps();
